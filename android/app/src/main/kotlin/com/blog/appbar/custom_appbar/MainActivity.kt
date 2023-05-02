6ca358fea1304a4948be0083248079afed88466a
quokka12 <znjzk0636@naver.com>
2023-05-03 03:31:14 +0900

    Initial commit

---
+++ android/app/src/main/kotlin/com/blog/appbar/custom_appbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.blog.appbar.custom_appbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/main_screen.dart
@@ -0,0 +1,35 @@
+import 'package:custom_appbar/screen/sub_screen.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../widget/appbar/main_app_bar.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(60), // 앱바 높이 조절
+          child: MainAppBar(),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          width: double.infinity,
+          height: double.infinity,
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const SubScreen()),
+              );
+            },
+            child: Text('서브 페이지로 이동'),
+          ),
+        ),
+      ),
+    );
+  }
+}
